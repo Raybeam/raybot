64f56843f45f52556ed8ebf1c64058c9caebcfdf
Kevin Kays <kkays@raybeam.com>
2019-05-03 10:05:50 -0400

    atme

---
+++ raybot/commands/atme.rb
@@ -0,0 +1,32 @@
+# Description:
+#   Pugme is the most important thing in your life
+#
+# Dependencies:
+#   None
+#
+# Configuration:
+#   None
+#
+# Commands:
+#   pug me - Receive a pug
+#   pug bomb N - get N pugs
+require 'net/http'
+require 'json'
+require 'redis'
+
+module RayBot
+  module Commands
+    class AtmeCommands < SlackRubyBot::Commands::Base
+      match (/^(?<name>@[a-zA-Z ]+) me$/) do |client, data, match|
+        client.say(channel: data.channel, text: match[:name])
+      end
+
+      match (/^(?<name>@[a-zA-Z ]+) bomb (?<count>\d+)$/) do |client, data, match|
+        count = [match[:count].to_i, 5].min
+        count.times do
+          client.say(channel: data.channel, text: match[:name])
+        end
+      end
+    end
+  end
+end
