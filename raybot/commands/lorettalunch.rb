a282ee6e51e30137a5455f211e1278a1c74b85c6
Robert Hollinger <rhollinger@raybeam.com>
2019-10-25 11:22:00 -0400

    fix naming

--- raybot/commands/lorettalunch.rb
+++ raybot/commands/lorettalunch.rb
@@ -8,18 +8,18 @@ module RayBot
         date = Time.now.strftime("%Y-%m-%d")
 
         meal_uri = URI.parse(base_url + date)
-        meal_http = Net::HTTP.new(uri.host, uri.port)
-        meal_request = Net::HTTP::Get.new(uri.request_uri)
-        meal_response = http.request(request)
-        meals = JSON.parse(response.body)
+        meal_http = Net::HTTP.new(meal_uri.host, meal_uri.port)
+        meal_request = Net::HTTP::Get.new(meal_uri.request_uri)
+        meal_response = meal_http.request(meal_request)
+        meals = JSON.parse(meal_response.body)
 
         specials_url = "http://lorettarestaurant.com/specials/?C=M;O=D"
 
         special_uri = URI.parse(specials_url)
-        special_http = Net::HTTP.new(uri.host, uri.port)
-        special_request = Net::HTTP::Get.new(uri.request_uri)
-        special_response = http.request(request)
-        specials = response.body.split("</tr>
+        special_http = Net::HTTP.new(special_uri.host, special_uri.port)
+        special_request = Net::HTTP::Get.new(special_uri.request_uri)
+        special_response = special_http.request(special_request)
+        specials = special_response.body.split("</tr>
         <tr>")
 
         meal_url = "Loretta isn't for lunch today!"
