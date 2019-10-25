444f471bf8f88cc6fac4c51304abaeb303e4ec37
Robert Hollinger <rhollinger@raybeam.com>
2019-10-25 15:06:12 -0400

    add feature to shame those that havent put in lunch yet

---
+++ raybot/commands/lunchshame.rb
@@ -0,0 +1,38 @@
+require "net/http"
+require 'nokogiri'
+
+module RayBot
+  module Commands
+    class LunchShame < SlackRubyBot::Commands::Base
+      match (/^.*(lunchshame).*$/) do |client, data, match|
+        base_url = "http://samaya.raybeam.com/meal_events/lunch_list?end=2000-01-01&start="
+        date = Time.now.strftime("%Y-%m-%d")
+
+        meal_uri = URI.parse(base_url + date)
+        meal_http = Net::HTTP.new(meal_uri.host, meal_uri.port)
+        meal_request = Net::HTTP::Get.new(meal_uri.request_uri)
+        meal_response = meal_http.request(meal_request)
+        meals = JSON.parse(meal_response.body)
+
+        waiting_on = []
+        for meal in meals
+          if meal["start"] == date
+            event_uri = URI.parse("http://samaya.raybeam.com" + meal["url"])
+            event_http = Net::HTTP.new(event_uri.host, event_uri.port)
+            event_request = Net::HTTP::Get.new(event_uri.request_uri)
+            event_response = meal_http.request(event_request)
+            event_html = Nokogiri::HTML.parse(event_response.body)
+            for person in event_html.children[1].children[3].children[3].children[7].children
+              if person.inner_html.include? "pending"
+                if person.inner_html.include? "first pending"
+                  waiting_on.append(person.children[1].content)
+                end
+              end
+            end
+          end
+        end
+        client.say(channel: data.channel, text: "WAITING ON: " + waiting_on.join(', '))
+      end
+    end
+  end
+end
