509f9212851589adf0bedbc8aa2709e066c3055c
Robert Hollinger <rhollinger@raybeam.com>
2019-10-25 12:43:27 -0400

    new key

--- raybot/commands/lorettalunch.rb
+++ raybot/commands/lorettalunch.rb
@@ -8,7 +8,7 @@ module RayBot
         redis = Redis.new(url: ENV['REDISTOGO_URL'])
         base_url = "http://samaya.raybeam.com/meal_events/lunch_list?end=2000-01-01&start="
         date = Time.now.strftime("%Y-%m-%d")
-        k = "lorettalunch" + date
+        k = "lorettalunch:" + date
 
         # Exit if the URL has already been found.
         break if redis.get(k)
