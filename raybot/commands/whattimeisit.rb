998dbfa654b081822652fedb73496b71e86183a5
alinden <alinden@raybeam.com>
2019-11-14 12:09:44 -0500

    timefix

--- raybot/commands/whattimeisit.rb
+++ raybot/commands/whattimeisit.rb
@@ -6,12 +6,22 @@ module RayBot
       match (/^(whattimeisit)|(.*([wW]hat(\s+.*)*\s+time(\s+.*)*\s+is(\s+.*)*\s+it).*)$/) do |client, data, match|
         redis = Redis.new(url: ENV['REDISTOGO_URL'])
         k = 'universal-time'
+        rk = 'return-time'
         time = (redis.get(k)).to_i
         unless time
           time = 0
         end
         redis.set(k, (time + 1).to_s)
-        client.say(channel: data.channel, text: time)
+        unless time % 51 == 0
+          unless redis.get(rk) && redis.get(rk) > time
+            client.say(channel: data.channel, text: time)
+          end
+        else
+          return_time = time + rand(20)
+          redis.set(rk, return_time)
+          post_office_message = "running to the post office - back at #{return_time}"
+          client.say(channel: data.channel, text: post_office_message)
+        end
       end
     end
   end
